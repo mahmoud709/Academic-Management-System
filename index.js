7c8a33ec9a7c1497524f9ff9a32ab8f44ff5c4c4
mahmoud <mahmoud123@gmail.com>
2023-05-02 23:35:32 +0300

    Import handlebars to project

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import { engine } from 'express-handlebars';
+
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = 3000;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.get('/',(req, res) => {
     res.send("<h2>Hello from NodeJS</h2>")
