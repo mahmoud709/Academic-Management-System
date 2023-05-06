bc8150e8aee0f5d973f285efd68f90be1532de0b
Your Name <you@example.com>
2023-05-06 17:38:58 +0200

    added accounts feature

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import { engine } from 'express-handlebars';
 import * as dotenv from 'dotenv';
 import route from './routes/subjects.js';
+import studentsAcc from './routes/studentsAcc.js';
+import doctorsAcc from './routes/doctorsAcc.js';
 
 mongoose.connect('mongodb://127.0.0.1:27017/department')
 dotenv.config()
@@ -13,6 +15,8 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 app.use('/subjects',route)
+app.use('/studentAcc' , studentsAcc);
+app.use('/doctorAcc' , doctorsAcc);
 app.listen(3000, () => {
     console.log('app is running')
 });
