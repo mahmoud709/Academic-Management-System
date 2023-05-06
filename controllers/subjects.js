f37fc4a9a8e85cc789b1b7bcbb891bd27a63c491
mahmoud <mahmoud123@gmail.com>
2023-05-06 23:44:10 +0300

    Building pages for admin and department and add departments

--- controllers/subjects.js
+++ controllers/subjects.js
@@ -27,3 +27,27 @@ export const show = async (req, res) => {
     const singlesubject = await subjectmodel.findById(_id).populate('department').lean();
     res.render('subjects/show', { singlesubject })
 }
+
+export const alldeps = async (req, res) => {
+    const alldepartments = await departmentmodel.find({}, { name: 1 }).lean();
+    const deps=[...new Set(alldepartments)]
+    res.render('departments/department', { deps });
+}
+
+export const addepartment =(req, res) => {
+    res.render('departments/adddepartment')
+}
+
+export const createdepartment = async (req, res) => {
+    const { DepartmentName, DepartmentCode } = req.body;
+    const existingDepartment = await departmentmodel.findOne({ code: DepartmentCode });
+    if (existingDepartment) {
+        res.status(409).send("Department with this code already exists");
+    } else {
+        await departmentmodel.create({
+            name: DepartmentName,
+            code: DepartmentCode,
+        });
+        res.redirect('/admin/departments');
+    }
+}
--- routes/subjects.js
+++ routes/subjects.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { admin,create, index, store ,show} from "../controllers/subjects.js";
+import { admin,create, index, store ,show,addepartment,alldeps,createdepartment} from "../controllers/subjects.js";
 
 const route = new Router();
 route.get('/',admin )
 route.get('/subjects', index);
 route.get('/subjects/create', create);
 route.post('/subjects', store);
-route.get('/subjects/:_id',show)
+route.get('/subjects/:_id', show)
+route.get('/departments',alldeps)
+route.get('/adddep', addepartment)
+route.post('/departments',createdepartment)
 export default route;
 
 
