88a41d9def2e4527f64c95049f5d4bf898adb13d
mahmoudshalaby <75578092+mahmoud709@users.noreply.github.com>
2023-05-07 22:35:50 +0300

    Edit index.js with changes in .env

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { engine } from 'express-handlebars';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
+dotenv.config();
+
 import route from './routes/subjects.js';
 import studentsAcc from './routes/studentsAcc.js';
 import doctorsAcc from './routes/doctorsAcc.js';
 
-mongoose.connect('mongodb://127.0.0.1:27017/department')
-dotenv.config()
+mongoose.connect(process.env.mongoConnection)
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-// use the route middleware with the /admin prefix
 app.use('/admin', route);
-
 app.use('/studentAcc' , studentsAcc);
 app.use('/doctorAcc' , doctorsAcc);
 
-app.listen(3000, () => {
-    console.log('app is running')
+app.listen(process.env.port, () => {
+    console.log(`app is running in http://localhost:${process.env.port}`)
 });
