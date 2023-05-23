176d2c275e3afde158612f9a3c3e148d973c4a8e
ahmed <ahmed115@gmail.com>
2023-05-23 20:46:58 +0300

    minisystem

---
+++ README.md
@@ -0,0 +1,2 @@
+# Academic-Management-System
+The Academic Management System is an integrated platform designed to manage and streamline academic processes in educational institutions. It provides a centralized system for students, teachers, and administrators to manage their academic activities, including course registration, attendance tracking, grade reporting, and more.
---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { engine } from 'express-handlebars';
+import dotenv from 'dotenv';
+import methodOveride from 'method-override';
+dotenv.config();
+
+import route from './routes/subjects.js';
+import studentsAcc from './routes/studentsAcc.js';
+import doctorsAcc from './routes/doctorsAcc.js';
+import { authentication } from './middleware/authentication.js';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.js'
+
+mongoose.connect(process.env.mongoConnection)
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOveride('_method'))
+app.use(cookieParser())
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use('/', authRoutes)
+app.use('/admin', route);
+app.use('/admin/studentAcc' , studentsAcc);
+app.use('/admin/doctorAcc' , doctorsAcc);
+
+app.listen(process.env.port, () => {
+    console.log(`app is running in http://localhost:${process.env.port}`)
+});
